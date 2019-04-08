afd96cb3f40daa8818047b5f302c42104b3ec08f
Jack Waeger <c3304734@uon.edu.au>
2019-04-08 03:15:47 +0000

    wote code

--- main.c
+++ main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
+#include<string.h>
+#include<stblib.h>
 
-int main () {
-asdf
-asdf
-afdd
-return 0;
+void caesarCipher(char* plainText, int key);
+
+int main(void) {
+    
+    int key = 1
+    char plainText[101];
+    printf("Please enter text your want to encript: ");
+    
+    fgets(plainText, sizeof(plainText), stdin);
+    
+    printf("the ciphered text is: ");
+    
+    
+    system("pause");
+}
+
+void ceaserCipher(char* plainTextm int key) {
+    int i = 0;
+    int cypherValue;
+    char cyher;
+    
+    while(plainText[i] ` = '\0' && strlen(plainText)-1 > i)
+        cypherValue = ((int)plainText[i] - 97 + key) mod 26 + 97); 
+        cypher = (char)(cypherValue)
+          
+        
+}
+
 }
